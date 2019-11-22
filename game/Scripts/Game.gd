58faa0dc875cbdff0823a454664a8b5749fb2c9f
agustafson19 <40578471+agustafson19@users.noreply.github.com>
2019-11-22 15:52:40 -0600

    Begun Dev. on Rooms
    Game scene now stateically loads rooms based upon pre-determined input.

--- game/Scripts/Game.gd
+++ game/Scripts/Game.gd
@@ -1,12 +1,31 @@
 extends Node
 
+const CELL_SIZE = 2304
 var player_bullet
 var enemy_bullet
 var bullet
+var room
 
 func _ready():
 	player_bullet = load("res://Objects/PlayerBullet.tscn")
 	enemy_bullet = load("res://Objects/EnemyBullet.tscn")
+	set_room(Vector2(0, 0), 11)
+	set_bridge(Vector2(1, 0), 0)
+	set_room(Vector2(1, 0), 13)
+
+func set_room(pos, n):
+	pos *= CELL_SIZE
+	room = load("res://Rooms/Room" + str(n) + ".tscn").instance()
+	room.position = pos
+	add_child(room)
+	
+func set_bridge(pos, n):
+	pos *= CELL_SIZE
+	pos.x -= CELL_SIZE / 3
+	pos.y -= CELL_SIZE / 3
+	room = load("res://Rooms/Bridge" + str(n) + ".tscn").instance()
+	room.position = pos
+	add_child(room)
 
 func _player_shoot(bullet_speed, bullet_damage):
 	bullet = player_bullet.instance()
