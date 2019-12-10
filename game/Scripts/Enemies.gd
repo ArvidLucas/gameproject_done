9d724c95d5da77b2c86a23550e2c300b9357b091
agustafson19 <40578471+agustafson19@users.noreply.github.com>
2019-12-10 10:10:23 -0600

    Added Gates
    Player must clear a room before exiting it.

--- game/Scripts/Enemies.gd
+++ game/Scripts/Enemies.gd
@@ -1,14 +1,27 @@
 extends Node2D
 
 var active
+var gated
+var gate
 
 func _ready():
 	active = false
+	gated = false
+	var size = get_parent().size
+	gate = load("res://Rooms/Size" + str(size) + "/Gate.tscn").instance()
+
+func _process(delta):
+	if gated and get_child_count() == 2:
+		gated = false
+		gate.queue_free()
 
 func _body_entered(body):
-	if body.is_in_group("Player"):
-		active = true
-	elif body.is_in_group("Player_Bullet") and !active:
-		body.queue_free()
-	elif body.is_in_group("Enemy_Bullet") and !active:
-		body.queue_free()
+	if !active:
+		if body.is_in_group("Player"):
+			active = true
+			gated = true
+			add_child(gate)
+		elif body.is_in_group("Player_Bullet"):
+			body.queue_free()
+		elif body.is_in_group("Enemy_Bullet"):
+			body.queue_free()
